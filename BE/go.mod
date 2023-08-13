61d2d6d627915e6bb9bf936f5641c0215490a03c
WahidinAji <a17wahidin@gmail.com>
2023-08-13 10:46:16 +0700

    ranger merah siap berubah

--- BE/go.mod
+++ BE/go.mod
@@ -6,18 +6,22 @@ require (
 	github.com/jackc/pgx/v5 v5.4.3
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/rs/zerolog v1.30.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/rs/zerolog v1.30.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.25.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
--- BE/main.go
+++ BE/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/urfave/cli/v2"
 	"net"
 	"net/http"
 	"os"
@@ -24,56 +25,111 @@ type ErrorResponse struct {
 }
 
 func main() {
-	config, err := GetConfig()
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to get config")
+
+	app := &cli.App{
+		Name:  "teknum-conf",
+		Usage: "say a greeting",
+		Commands: []*cli.Command{
+			{
+				Name: "server",
+				Action: func(cCtx *cli.Context) error {
+					config, err := GetConfig()
+					if err != nil {
+						log.Fatal().Err(err).Msg("Failed to get config")
+					}
+					conn, err := pgxpool.New(
+						context.Background(),
+						fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", config.DBUser, config.DBPassword, config.DBHost, config.Port, config.DBName),
+					)
+					if err != nil {
+						log.Fatal().Err(err).Msg("Failed to connect to database")
+					}
+					defer conn.Close()
+					e := echo.New()
+
+					userDomain := user.New(conn)
+					// TODO: move handler out from the main function
+					e.POST("users", func(c echo.Context) error {
+						payload := user.CreateParticipantRequest{}
+						if err := c.Bind(&payload); err != nil {
+							return err
+						}
+
+						err := userDomain.CreateParticipant(c.Request().Context(), payload)
+						if err != nil {
+							if errors.Is(err, user.ErrValidation) {
+								return c.JSON(400, ErrorResponse{Error: err.Error()})
+							}
+
+							return c.JSON(500, ErrorResponse{Error: "Internal server error"})
+						}
+
+						return c.NoContent(201)
+					})
+
+					exitSig := make(chan os.Signal, 1)
+					signal.Notify(exitSig, os.Interrupt, os.Kill)
+
+					go func() {
+						<-exitSig
+						ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+						defer cancel()
+
+						if err := e.Shutdown(ctx); err != nil {
+							log.Error().Err(err).Msg("Failed to shutdown server")
+						}
+					}()
+
+					if err := e.Start(net.JoinHostPort("", config.Port)); err != nil && !errors.Is(err, http.ErrServerClosed) {
+						log.Fatal().Err(err).Msg("Failed to start server")
+					}
+					return nil
+				},
+			},
+			{
+				Name: "migrate",
+				Action: func(cCtx *cli.Context) error {
+					//TODO: do migration here
+					log.Info().Msg("Migrating database")
+					return nil
+				},
+			},
+			{
+				Name: "dump-attendees",
+				Action: func(cCtx *cli.Context) error {
+					//TODO: get attendees from postgres and dump to csv in stdout
+					log.Info().Msg("Dumping attendees")
+					return nil
+				},
+			},
+			{
+				Name:      "blast-email",
+				Usage:     "blast-email [template] [file list destination of emails]",
+				ArgsUsage: "[template] [file list destination of emails]",
+				Action: func(cCtx *cli.Context) error {
+					templateArg := cCtx.Args().Get(0)
+					emailList := cCtx.Args().Tail()
+
+					if templateArg == "" {
+						log.Fatal().Msg("Template is required")
+					}
+
+					if len(emailList) == 0 {
+						log.Fatal().Msg("Email list is required for blasting email minimum 1 email")
+					}
+
+					for _, email := range emailList {
+						log.Info().Msgf("Sending email to %s", email)
+					}
+
+					//TODO: send email to all attendees. Implement email your logic here
+					log.Info().Msg("Blasting email")
+					return nil
+				},
+			},
+		},
 	}
-
-	conn, err := pgxpool.New(
-		context.Background(),
-		fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", config.DBUser, config.DBPassword, config.DBHost, config.Port, config.DBName),
-	)
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to connect to database")
-	}
-	defer conn.Close()
-
-	e := echo.New()
-
-	userDomain := user.New(conn)
-	// TODO: move handler out from the main function
-	e.POST("users", func(c echo.Context) error {
-		payload := user.CreateParticipantRequest{}
-		if err := c.Bind(&payload); err != nil {
-			return err
-		}
-
-		err := userDomain.CreateParticipant(c.Request().Context(), payload)
-		if err != nil {
-			if errors.Is(err, user.ErrValidation) {
-				return c.JSON(400, ErrorResponse{Error: err.Error()})
-			}
-
-			return c.JSON(500, ErrorResponse{Error: "Internal server error"})
-		}
-
-		return c.NoContent(201)
-	})
-
-	exitSig := make(chan os.Signal, 1)
-	signal.Notify(exitSig, os.Interrupt, os.Kill)
-
-	go func() {
-		<-exitSig
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-		defer cancel()
-
-		if err := e.Shutdown(ctx); err != nil {
-			log.Error().Err(err).Msg("Failed to shutdown server")
-		}
-	}()
-
-	if err := e.Start(net.JoinHostPort("", config.Port)); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		log.Fatal().Err(err).Msg("Failed to start server")
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal().Err(err).Msg("Failed to run app")
 	}
 }

9e5541989ad319f13ad689a99f2579b7ce91cc3e
Reinaldy Rafli <aldy505@proton.me>
2023-08-13 15:31:14 +0700

    chore(backend): add three dots for spreading the input

--- backend/conformed_logger.go
+++ backend/conformed_logger.go
@@ -5,9 +5,9 @@ import "github.com/rs/zerolog/log"
 type conformedLogger struct{}
 
 func (c *conformedLogger) Printf(format string, v ...interface{}) {
-	log.Printf(format, v)
+	log.Printf(format, v...)
 }
 
 func (c *conformedLogger) Fatalf(format string, v ...interface{}) {
-	log.Fatal().Msgf(format, v)
+	log.Fatal().Msgf(format, v...)
 }

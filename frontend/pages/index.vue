3eeed2f4009cbf1b6bf7f83933eac55d19040c24
Ahmad Saugi <saugi.dev@gmail.com>
2023-08-20 10:39:02 +0700

    feat(frontend): code of conduct page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -20,9 +20,10 @@ useHead({
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5M12 2a7 7 0 0 1 7 7c0 5.25-7 13-7 13S5 14.25 5 9a7 7 0 0 1 7-7m0 2a5 5 0 0 0-5 5c0 1 0 3 5 9.71C17 12 17 10 17 9a5 5 0 0 0-5-5Z"/></svg>
                                 <span>Depok, Indonesia</span>
                             </h3>
+                            <Btn class="mt-15" size="xl">Save my spot!</Btn>
                         </div>
                         <div class="card-right flex flex-grow-1 justify-end items-center">
-                            <img src="@/assets/images/logo-white.png" alt="TeknumConf logo" class="w-80  hidden rounded-full">
+                            <img src="@/assets/images/logo-white.png" alt="TeknumConf logo" class="w-100 hidden lg:block rounded-full">
                         </div>
                     </div>
                 </div>
